8a0872720afab1c385450c3cf44ae49d9f9ad2eb
Juan Bindez <juanbindez780@gmail.com>
2024-10-01 17:14:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import requests
 import re
 import sys
 
-# Função para baixar o arquivo de áudio
 def download_audio(url, filename):
     try:
         response = requests.get(url, stream=True)
-        response.raise_for_status()  # Levanta um erro para respostas ruins
+        response.raise_for_status()
         
         with open(filename, 'wb') as file:
             for chunk in response.iter_content(chunk_size=1024):
@@ -16,14 +15,11 @@ def download_audio(url, filename):
     except requests.exceptions.RequestException as e:
         print(f"Ocorreu um erro ao baixar o áudio: {e}")
 
-# Função para extrair e baixar o áudio
 def extract_and_download_audio(page_url):
     try:
-        # Obtém o conteúdo HTML da página
         response = requests.get(page_url)
-        response.raise_for_status()  # Levanta um erro para respostas ruins
+        response.raise_for_status()
         
-        # Procura pela URL do áudio usando regex
         pattern = r'let dubbedAudioURL = "(.*?)";'
         match = re.search(pattern, response.text)
         
@@ -31,21 +27,17 @@ def extract_and_download_audio(page_url):
             audio_url = match.group(1)
             print(f"URL do áudio encontrada: {audio_url}")
             
-            # Baixa o arquivo de áudio
             download_audio(audio_url, 'downloaded_audio.mp3')
         else:
             print("Nenhuma URL de áudio encontrada na variável 'dubbedAudioURL'.")
     except requests.exceptions.RequestException as e:
         print(f"Ocorreu um erro ao acessar a página: {e}")
 
-# Bloco principal para lidar com o argumento da linha de comando
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Uso: python script.py <URL>")
         sys.exit(1)
     
-    # Obtém a URL a partir do argumento da linha de comando
     page_url = sys.argv[1]
     
-    # Chama a função para extrair e baixar o áudio
     extract_and_download_audio(page_url)
